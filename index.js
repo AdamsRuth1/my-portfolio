bf3f3cf74b4c3511d51b89eca09deb79ab529b05
Adams ruth <adamsruth4080@gmail.com>
2024-01-12 06:06:02 -0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const toggleBtn = document.getElementById("hamburger");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
+const tl = gsap.timeline({defaults: {duration:3}})
+tl.fromTo('.display', {scale:0}, {scale:1})
+gsap.fromTo('.magic', {x: 500, opacity: 1}, {opacity: 1, x:0, duration: 50})
+gsap.fromTo('.logo', {x: 500, opacity: 1}, {opacity: 1, x:0, duration: 20})
+gsap.fromTo('.links', {x: 500, opacity: 1}, {opacity: 1, x:0, duration: 10})
+gsap.fromTo('.social-icons', {x: 800, opacity: 1}, {opacity: 1, x:0, duration: 20})
+// tl.fromTo(
+//     '.sidebar',
+//     { yPercent: -100, opacity: 0 },
+//     { yPercent: 0, opacity: 1 }
+//   );
+
 toggleBtn.addEventListener("click", function () {
   // if (sidebar.classList.contains("show-sidebar")) {
   //   sidebar.classList.remove("show-sidebar");
